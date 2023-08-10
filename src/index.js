f6097aa9bada17776f47777bd3453ff924e30729
liuqixing <liuqixing@dekuan.org>
2023-08-11 04:28:56 +0800

    Upgraded exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { StorageService } from "./services/storage/StorageService.js";
-import { PeerIdStorageService } from "./services/storage/PeerIdStorageService.js";
-import { SwarmKeyStorageService } from "./services/storage/SwarmKeyStorageService.js";
-export { StorageService, PeerIdStorageService, SwarmKeyStorageService }
+export { StorageService }
 
+import { PeerIdStorageService } from "./services/storage/PeerIdStorageService.js";
 import { PeerIdService } from "./services/PeerIdService.js";
-export { PeerIdService }
+export { PeerIdStorageService, PeerIdService }
+
+import { SwarmKeyStorageService } from "./services/storage/SwarmKeyStorageService.js";
+import { SwarmKeyService } from "./services/SwarmKeyService.js";
+export { SwarmKeyStorageService, SwarmKeyService }
+
 
 import { LogUtil } from "./utils/LogUtil.js";
 import { ObjectUtil } from "./utils/ObjectUtil.js";
