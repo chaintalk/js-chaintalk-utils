5ad67045612278bd6ae67ea247fdf8ede03abe0e
liuqixing <liuqixing@dekuan.org>
2023-08-30 19:01:25 +0800

    Added TestUtil

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { ObjectUtil } from "./utils/ObjectUtil";
 import { TypeUtil } from "./utils/TypeUtil";
 import { BufferUtil } from "./utils/BufferUtil";
 import { PageUtil } from "./utils/PageUtil";
+import { TestUtil } from "./utils/TestUtil";
 
-export { LogUtil, ObjectUtil, TypeUtil, BufferUtil, PageUtil }
+export { LogUtil, ObjectUtil, TypeUtil, BufferUtil, PageUtil, TestUtil }
