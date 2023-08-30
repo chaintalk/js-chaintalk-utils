4cb84e7fb4d40084111fb0defa2415054c049b36
liuqixing <liuqixing@dekuan.org>
2023-08-30 14:03:24 +0800

    Added new methods getRangeFromArrayByPage, getRangeFromArrayByIndex

--- src/utils/PageUtil.ts
+++ src/utils/PageUtil.ts
@@ -13,7 +13,7 @@ export class PageUtil
 	 *	@param defaultValue	{number}
 	 *	@returns {number}
 	 */
-	public static getSafePageNo( pageNo ?: number, defaultValue ?: number ) : number
+	public static getSafePageNo( pageNo ? : number, defaultValue ? : number ) : number
 	{
 		if ( undefined !== pageNo && pageNo > 0 )
 		{
@@ -33,7 +33,7 @@ export class PageUtil
 	 *	@param defaultValue	{number}
 	 *	@returns {number}
 	 */
-	public static getSafePageSize( pageSize ?: number, defaultValue ?: number ) : number
+	public static getSafePageSize( pageSize ? : number, defaultValue ? : number ) : number
 	{
 		if ( undefined !== pageSize && this.isValidPageSize( pageSize ) )
 		{
@@ -53,11 +53,69 @@ export class PageUtil
 	 */
 	public static isValidPageSize( value : any ) : boolean
 	{
-		if ( ! TypeUtil.isNumeric( value ) )
+		if ( !TypeUtil.isNumeric( value ) )
 		{
 			return false;
 		}
 
 		return value >= this.defaultPageSizeMin && value <= this.defaultPageSizeMax;
+	}
+
+	/**
+	 *	@param arr	{Array<*>}
+	 *	@param pageNo	{number}
+	 *	@param pageSize	{number}
+	 *	@returns {Array<*>}
+	 */
+	public static getRangeFromArrayByPage<T>( arr : Array<T>, pageNo : number, pageSize : number ) : Array<T>
+	{
+		if ( ! Array.isArray( arr ) || 0 === arr.length )
+		{
+			return [];
+		}
+
+		pageNo = this.getSafePageNo( pageNo );
+		pageSize = this.getSafePageSize( pageSize );
+		const startIndex : number = ( pageNo - 1 ) * pageSize;
+		const endIndex : number = Math.min( pageNo * pageSize, arr.length ) - 1;
+
+		return this.getRangeFromArrayByIndex<T>( arr, startIndex, endIndex );
+	}
+
+	/**
+	 *	@param arr		{Array<*>}
+	 *	@param startIndex	{number}
+	 *	@param endIndex		{number}
+	 *	@returns {Array<*>}
+	 */
+	public static getRangeFromArrayByIndex<T>( arr : Array<T>, startIndex : number, endIndex : number ) : Array<T>
+	{
+		if ( ! Array.isArray( arr ) || 0 === arr.length )
+		{
+			return [];
+		}
+		if ( startIndex < 0 )
+		{
+			startIndex = 0;
+		}
+		if ( endIndex >= arr.length )
+		{
+			endIndex = arr.length - 1;
+		}
+		if ( startIndex > endIndex )
+		{
+			return [];
+		}
+
+		//
+		//	const data = [10, 20, 30, 40, 50, 60, 70, 80];
+		//
+		// 	const startIndex = 2;
+		// 	const endIndex = 5;
+		//
+		// 	const extractedData = getRangeFromArray(data, startIndex, endIndex);
+		// 	console.log(extractedData); // Output: [30, 40, 50, 60]
+		//
+		return arr.slice( startIndex, endIndex + 1 );
 	}
 }
